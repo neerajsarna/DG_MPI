502bd888e6c8a34f4f222760c9bbfbb051f05a4b
neerajsarna <sarna@mathcces.rwth-aachen.de>
2018-07-10 20:45:30 +0200

    higher order results

--- job_submit.sh
+++ job_submit.sh
@@ -5,18 +5,18 @@
  
 ### File / path where STDOUT & STDERR will be written
 ###    %J is the job ID, %I is the array ID
-#BSUB -o log_files/2x1v_moments_Inflow_M4
+#BSUB -o log_files/2x1v_moments_Inflow_M14
  
 ### Request the time you need for execution in minutes
 ### The format for the parameter is: [hour:]minute,
 ### that means for 80 minutes you could also use this: 1:20
-#BSUB -W 1:00
+#BSUB -W 0:20
  
 ### Request memory you need for your job per PROCESS in MB
 #BSUB -M 2024
  
 ### Request the number of compute slots you want to use
-#BSUB -n 64
+#BSUB -n 4
  
 ### Use esub for Open MPI
 #BSUB -a openmpi
@@ -27,6 +27,6 @@ module load openmpi/1.10.2
 
 cd /home/ns179556/DG_MPI
  
-export FLAGS_MPI_BATCH="-np 8"
+export FLAGS_MPI_BATCH="-np 2"
 ### Execute your application
-mpirun $FLAGS_MPI_BATCH ./2x1v_Inflow.out 8 500 4
+mpirun $FLAGS_MPI_BATCH ./2x1v_Inflow.out 2 200 14
