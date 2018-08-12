6b5a90b6cf5c3c932e7c5a8f863c2447a5268126
neerajsarna <sarna@mathcces.rwth-aachen.de>
2018-08-12 09:21:21 +0200

    added high resolution advection results

--- job_submit.sh
+++ job_submit.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env zsh
  
 ### Job name
-#BSUB -J 2x1v_moments_Adp
+#BSUB -J 2D_advection_gaussian
  
 ### File / path where STDOUT & STDERR will be written
 ###    %J is the job ID, %I is the array ID
-#BSUB -o log_files/2x1v_moments_Inflow_Adp
+#BSUB -o log_files/2D_advection_gaussian
  
 ### Request the time you need for execution in minutes
 ### The format for the parameter is: [hour:]minute,
 ### that means for 80 minutes you could also use this: 1:20
-#BSUB -W 1:20
+#BSUB -W 5:00
  
 ### Request memory you need for your job per PROCESS in MB
-#BSUB -M 1000
+#BSUB -M 6000
  
 ### Request the number of compute slots you want to use
 #BSUB -n 16
@@ -29,4 +29,4 @@ cd /home/ns179556/DG_MPI
  
 export FLAGS_MPI_BATCH="-np 1"
 ### Execute your application
-mpirun $FLAGS_MPI_BATCH ./2D_advection_gaussian.out 16 10
+mpirun $FLAGS_MPI_BATCH ./2D_advection_gaussian.out 16 5
